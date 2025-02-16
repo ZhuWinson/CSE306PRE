4cd846db81331af9fca08d2d263ffcaf41f9658a
j4jet <jrex4jet@gmail.com>
2025-02-16 16:21:53 -0500

    Set up basic functionality of "-f" flag. Added an else that will catch undefined behavior. I also modified the activity file to format it a bit differently.

--- csv.c
+++ csv.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //Note: LEX02 has some help for files
 
@@ -23,5 +24,49 @@ int main(int argc, char * argv[])
     if (file == NULL)
         {
             printf("Error opening file\n");
+            return EXIT_FAILURE;
         }
+    
+    // -f
+    char* ef = "-f";
+    if ( strcmp(argv[1], ef) == 0){
+        //keeps count of the fields
+        int columns = 0;
+        //keeps count of the rows
+        int rows = 0;
+        //char buffer to store the contents of the csv file
+        char buffer[1024];
+
+        while(fgets(buffer, 1024, file)){
+            rows++;
+
+            //we only care about row 1
+            if (rows == 1){
+                //split on commas
+                char* value = strtok(buffer, ", ");
+
+                //count the number of fields
+                while(value){
+                    value = strtok(NULL, ", ");
+                    columns++;
+                }
+            }
+        }
+
+        //print the number of fields
+        printf("%d",columns);
+
+        //close the file
+        fclose(file);
+
+        //exit
+        return EXIT_SUCCESS;
+    }
+
+    // temp response
+    else
+    {
+        printf("Undefined\n");
+        return EXIT_FAILURE;
+    } 
 }
